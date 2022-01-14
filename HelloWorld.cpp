3a6d1b28dc70a1be76103230c63149afa17194c0
milos <milos.andjelkovic@redtork-solutions.com>
2022-01-14 10:25:52 +0100

    Ispravka 1

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int main()
 {
 
-    vector<string> msg {"Hello100-xx1111", "C++", "World", "from", "VS Code", "and the C++ extension!"}
-
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"}
+    // ovo je bila greska
 
     for (const string& word : msg)
     {
