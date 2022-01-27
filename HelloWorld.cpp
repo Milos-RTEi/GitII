3b1bffce3630d0f83c3ddc8015487ceb2ef0d326
milos <milos.andjelkovic@redtork-solutions.com>
2022-01-27 21:43:42 +0100

    opet komit

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,37 +7,10 @@ using namespace std;
 int main()
 {
 
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"}
-    // ovo je bila greska
+    int A = 11;
+    int B = 25;
 
-    /*
-
-    dodali smo ovde jos koda -----AAAA -CCCCC -DDDD
-
-    */
-
-
-    /*
-
-    ovo je sada subvarijanta varijante 1
-    */
-
-    /*
-
-    ovo sada dodajemo u WorkFlow-1
-    ovo sada dodajemo u WorkFlow-1 num.2
-    ovo sada dodajemo u WorkFlow-1 num.33
-    */
-
-
-    /*
-
-    ovo sada dodajemo u WorkFlow-1-1
-    ovo sada dodajemo u WorkFlow-1-1 num.2
-    ovo sada dodajemo u WorkFlow-1-1 num.3
-
-    */
-
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
     for (const string& word : msg)
     {
